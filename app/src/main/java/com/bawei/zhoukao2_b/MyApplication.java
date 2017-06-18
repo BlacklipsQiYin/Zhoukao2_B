7061f16e5bf855bd7ab08c379e3f2fa3c79d5340
fuxuewei <931865616@qq.com>
2017-06-18 21:33:49 +0800

    Initial commit

---
+++ app/src/main/java/com/bawei/zhoukao2_b/MyApplication.java
@@ -0,0 +1,15 @@
+package com.bawei.zhoukao2_b;
+
+import android.app.Application;
+
+/**
+ * Created by DELL on 2017/6/18.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
